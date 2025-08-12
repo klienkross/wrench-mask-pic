092417f0ee491061b0050a101bbb6eb687c2d57c
Kleinkross <151308137+klienkross@users.noreply.github.com>
2025-08-12 14:41:33 +0000

    .

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import machine
+import utime
 
 def main():
-  print("Welcome to RT-Thread MicroPython!")
-
+    led = machine.Pin(25, machine.Pin.OUT)
+    
+    while True:
+        led.value(1)
+        utime.sleep(1)
+        led.value(0)
+        utime.sleep(1)
+    
 if __name__ == '__main__':
-  main()
+    main()
+
 
